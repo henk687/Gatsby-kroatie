d7aadfac9034a8e57cf2e2f2bd4e00a46bd53854
henk687 <henkleenheer@upcmail.nl>
2021-01-12 23:11:15 +0100

    update

--- src/components/Email.js
+++ src/components/Email.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from "styled-components"
-import EmailBg from "../assets/images/kornati2.jpg"
+import EmailBg from "../assets/images/kornati.jpg"
 import { Button } from './Button'
 
 const Email = () => {
