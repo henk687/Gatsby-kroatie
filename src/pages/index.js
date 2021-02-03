9b89a81148a93f145c99680adf802acb03df6026
henk687 <henkleenheer@upcmail.nl>
2021-02-03 11:42:16 +0100

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import Email from "../components/Email"
 import Hero from "../components/Hero"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -24,8 +23,11 @@ const IndexPage = () => (
       </svg>
     </div>
     <Stats />
-    
-    <Email />
+    <div className="wave-white">
+      <svg className="svgwave" xmlns="http://www.w3.org/2000/svg" viewBox="-300 0 950 270" >
+      <path d="M-314,267 C105,364 400,100 812,279" fill="none" stroke="#020312" strokeWidth="120" strokeLinecap="round"/>
+      </svg>
+    </div>
   </Layout>
 )
 
